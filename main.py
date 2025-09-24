356d2f93ac90b4a81fe7bf55650691c9d64c92c0
Valentyn Zhuravlov <nataliyasvietohor@MacBook-Air---Valentyn.local>
2025-09-24 18:32:34 +0200

    Update main.py and .gitignore: English comments and ignore unnecessary files

--- main.py
+++ main.py
@@ -10,20 +10,14 @@ df = pd.read_csv('Competitions.csv')
 # Filter rows where id == 17592
 id = df[df['Slug'] == 'connectx']['Id']
 
-# Display the result
-#print(id)
-
 # Load the episodes dataset
 episodes_df = pd.read_csv('Episodes.csv')  # adjust the path if needed
 
 # Filter rows where competitionId == 17592
 filtered_episodes = episodes_df[episodes_df['CompetitionId'] == id.item()]
 
-# Display the filtered episodes
-#print(filtered_episodes)
-
 # Get the list of episode IDs
-EpisodId = filtered_episodes['Id'].tolist()
+EpisodeId = filtered_episodes['Id'].tolist()
 
 # Create outpu_folder for episods results
 OUTPUT_DIR = Path("Episods_output")
@@ -32,9 +26,9 @@ LOWEST_SCORE_THRESH = 2000
 EPISODE_LIMIT_SIZE = 10
 
 # Function for  saving the episods results in json format
-def saveEpisode(EpisodId:int):
+def saveEpisode(EpisodeId:int):
     # Create URL for request
-    GET_URL = f"https://www.kaggleusercontent.com/episodes/{EpisodId}.json"
+    GET_URL = f"https://www.kaggleusercontent.com/episodes/{EpisodeId}.json"
     # request
     re = requests.get(GET_URL)
     if re.status_code == 200:
@@ -42,14 +36,14 @@ def saveEpisode(EpisodId:int):
             # save replay
             replay = re.json()
             OUTPUT_DIR.mkdir(exist_ok=True)
-            with open(OUTPUT_DIR / f'{EpisodId}.json', 'w') as f:
+            with open(OUTPUT_DIR / f'{EpisodeId}.json', 'w') as f:
                 json.dump(replay, f)
-            print(f"Эпизод {EpisodId} успешно сохранён.")
+            print(f"Episode {EpisodeId} successfully saved.")
         except Exception as e:
-            print(f"Ошибка декодирования JSON для эпизода {EpisodId}: {e}")
+            print(f"JSON decode error for episode {EpisodeId}: {e}")
     else:
-        print(f"Ошибка запроса для эпизода {EpisodId}: статус {re.status_code}")
+        print(f"Request error for episode {EpisodeId}: status {re.status_code}")
 
 # Launch and Save the first 10 episodes  
-for eid in EpisodId[:EPISODE_LIMIT_SIZE]:
+for eid in EpisodeId[:EPISODE_LIMIT_SIZE]:
     saveEpisode(eid)
