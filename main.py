80deab54827afb2089db9996db07e5a4b3e1f2d8
Valentyn2025 <zhuravlovVY24@gmail.com>
2025-09-30 19:50:00 +0200

    DB for downloaded ids was created. List of downloaded was fixed and loop for duplication validation was fixed.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import requests as requests
 import json
 from pathlib import Path
 import os
+import sqlite3
 
 # Installing Competitions.csv
 df = pd.read_csv('Competitions.csv')
@@ -23,23 +24,34 @@ EpisodeId = filtered_episodes['Id'].tolist()
 # Create outpu_folder for episodes results
 OUTPUT_DIR = Path("Episodes_output")
 
+#Create DataBase
+DB_PATH = "downloaded_episodes_id.db"
+
+#Create DB and table
+conn = sqlite3.connect(DB_PATH)
+cursor = conn.cursor()
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS downloaded (
+    episodeId INTEGER PRIMARY KEY,
+    date TEXT,
+    local_path TEXT
+)
+""")
+
+conn.commit()
+
 LOWEST_SCORE_THRESH = 2000
 EPISODE_LIMIT_SIZE = 15
 
+#List of downloaded id's Beginning-----------------------------------
 # Get the list of ids' of all files from folder Episodes_output
 downloaded_files = {f for f in os.listdir(OUTPUT_DIR)
                     if f.endswith('.json')}
 downloaded_id = {os.path.splitext(f)[0] for f in downloaded_files}
-
+#List of downloaded id's End-----------------------------------------
 
 # Function for  saving the episodes results in json format
 def save_episode(episode_id: int):
-    # Duplicates verification
-    filepath = OUTPUT_DIR / f"{episode_id}.json"
-    if filepath.exists():
-        print(f"Episode {episode_id} already downloaded. Skipping.")
-        return
-
     # Create URL for request
     get_url = f"https://www.kaggleusercontent.com/episodes/{episode_id}.json"
     # request
@@ -55,13 +67,34 @@ def save_episode(episode_id: int):
 
             # Append new ids in the downloaded list of episodes
             downloaded_id.add(str(episode_id))
-
         except Exception as e:
             print(f"JSON decode error for episode {episode_id}: {e}")
     else:
         print(f"Request error for episode {episode_id}: status {re.status_code}")
 
+# Transform EpisodeId to str to compare with downloaded_id
+episode_ids_all = [str(eid) for eid in EpisodeId]
+
+# Comparing episode_ids_all with downloaded_id
+remaining_ids = [eid for eid in episode_ids_all if eid not in downloaded_id]
+
+# Limit download size to EPISODE_LIMIT_SIZE
+to_download = remaining_ids[:EPISODE_LIMIT_SIZE]
 
 # Launch and Save the first 15 episodes
-for eid in EpisodeId[:EPISODE_LIMIT_SIZE]:
-    save_episode(eid)
+for eid in to_download:
+    # Duplicates verification
+    filepath = OUTPUT_DIR / f"{eid}.json"
+    if filepath.exists():
+        print(f"Episode {eid} already downloaded. Skipping.")
+        continue # skip function launch
+    save_episode(int(eid))
+
+# Transform Set downloaded_id into DataFrame
+downloaded_id_df = pd.DataFrame({"Downloaded_Episode_id": list(downloaded_id)})
+
+# Save into csv
+downloaded_id_df.to_csv("Downloaded_Episodes_id.csv", index=False)
+
+# Close connection
+conn.close()
